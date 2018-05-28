b1776d547b1d0b8fd60e24192d740daf1b6e6c05
rafaelss <computacao.rafael@gmail.com>
2018-05-28 12:17:09 -0300

    17. And here is our Data Preprocessing Template!

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -2,22 +2,23 @@
 
 # Importando os dados
 dataset = read.csv('Data.csv')
+# dataset = dataset[, 2:3]
 
 # Tratando os dados faltantes
-dataset$Age = ifelse(is.na(dataset$Age), # Se nao tem o dado
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), # Usa a media
-                     dataset$Age) # Senao usa o proprio dado
-dataset$Salary = ifelse(is.na(dataset$Salary), # Se nao tem o dado
-                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), # Usa a media
-                        dataset$Salary) # Senao usa o proprio dado
+# dataset$Age = ifelse(is.na(dataset$Age), # Se nao tem o dado
+#                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), # Usa a media
+#                     dataset$Age) # Senao usa o proprio dado
+# dataset$Salary = ifelse(is.na(dataset$Salary), # Se nao tem o dado
+#                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), # Usa a media
+#                        dataset$Salary) # Senao usa o proprio dado
 
 # Codificando dados de categorias (qualitativos)
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased,
-                         levels = c('No', 'Yes'),
-                         labels = c(0, 1))
+# dataset$Country = factor(dataset$Country,
+#                         levels = c('France', 'Spain', 'Germany'),
+#                         labels = c(1, 2, 3))
+# dataset$Purchased = factor(dataset$Purchased,
+#                         levels = c('No', 'Yes'),
+#                         labels = c(0, 1))
 
 # Dividindo o conjunto de dados entre Treinamento e Teste
 # install.packages('caTools')
@@ -28,5 +29,5 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Colocando todos os dados na mesma escala (Feature Scaling)
-training_set[, 2:3] = scale(training_set[, 2:3])
-test_set[, 2:3] = scale(test_set[, 2:3])
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -11,26 +11,27 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Tratando os dados faltantes
-from sklearn.preprocessing import Imputer
+"""from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3]) # pegando colunas 1 e 2 - superior excludente
 X[:, 1:3] = imputer.transform(X[:, 1:3])
-
+"""
 # Codificando dados de categorias (qualitativos)
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
-
+"""
 # Dividindo o conjunto de dados entre Treinamento e Teste
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Colocando todos os dados na mesma escala (Feature Scaling)
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+"""
